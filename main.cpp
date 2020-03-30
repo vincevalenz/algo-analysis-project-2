a1b02f56be070dd54f463bd05a90df845ad6c222
AriEncarnacion <ari.encarnacion.95@gmail.com>
2020-03-30 15:01:04 -0700

    testing nodes vector

--- main.cpp
+++ main.cpp
@@ -33,6 +33,15 @@ int main(int argc, char *argv[]) {
     }
     std::cout << "\n------------------\n\n";
 
+    std::cout << "testing nodes\n";
+    std::vector< std::vector<int> > nodes = g.getNodes();
+    for (int i = 0; i < nodes.size(); i++) {
+        for (int j = 0; j < nodes[i].size(); j++)
+            std::cout << nodes[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+
     //testing optimal path -- in progress
     g.getOptimalPath();
 }
