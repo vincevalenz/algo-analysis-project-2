a1d97ff0d63ec7dec1720ee2862204ff01422893
AriEncarnacion <ari.encarnacion.95@gmail.com>
2020-03-29 18:48:07 -0700

    fixed errors

--- RentalForm.hpp
+++ RentalForm.hpp
@@ -10,15 +10,13 @@
 
 class RentalForm {
 public:
-    RentalForm(): name{}, startDate{-1}, endDate{-1}, amount{-1}{}
-    RentalForm(std::string n, int s, int e, int a): name{n}, startDate{s}, endDate{e}, amount{a}{}
-    std::string getName() { return name; }
+    RentalForm(): startDate{-1}, endDate{-1}, amount{-1}{}
+    RentalForm(int s, int e, int a) : startDate{s}, endDate{e}, amount{a}{}
     int getStartDate() { return startDate; }
     int getEndDate() { return endDate; }
     int getAmount() { return amount; }
 
 private:
-    std::string name;
     int startDate;
     int endDate;
     int amount;
