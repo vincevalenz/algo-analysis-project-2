0181994b84bf4c467fe8666a4f4a940c41c59af9
AriEncarnacion <ari.encarnacion.95@gmail.com>
2020-03-30 18:20:56 -0700

    fixed build matrix

--- Graph.cpp
+++ Graph.cpp
@@ -44,11 +44,11 @@ void Graph::buildMatrix() {
                 clientMatrix[i+1].at(k+1) = clients.at(i).getAmount();
             }
     }
-
+print();
     //connect start node
     //traverses matrix
     bool noIn = true;
-    for(int col = 1; col < matrixSize - 2; col++) {
+    for(int col = 1; col < matrixSize - 1; col++) {
         for (int row = 0; row < matrixSize - 1; row++) {
             int x = clientMatrix[row].at(col);
             if(clientMatrix[row].at(col) != -1) {
@@ -59,6 +59,8 @@ void Graph::buildMatrix() {
             clientMatrix[0].at(col) = 0;
         }
         noIn = true;
+        print();
+        std::cout<<"------\n";
     }
 
     //connect end node
@@ -79,33 +81,12 @@ void Graph::buildMatrix() {
 }
 
 void Graph::getOptimalPath() {
-    std::cout << "Testing Graph::getOptimalPath()\n";
-    for (int i = 0; i < clients.size(); i++) {
-        std::cout << "client [" << i << "]:\n";
-        std::cout << "   start: " << clients.at(i).getStartDate() << '\n';
-        std::cout << "   end: " << clients.at(i).getEndDate() << '\n';
-        std::cout << "   pay: " << clients.at(i).getAmount() << '\n';
-    }
 
-    //build rev_P
-    std::vector<RentalForm> rev_P;
-    RentalForm end(INFINITY, INFINITY, 0);
-    rev_P.push_back(end);
-    for (int i = clients.size() - 1; i >= 0; i--) {
-        rev_P.push_back(clients.at(i));
-    }
-    RentalForm start(0, 0, 0);
-    rev_P.push_back(start);
-
-
-    std::cout << "rev_P vector ------\n";
-    for (int i = 0; i < rev_P.size(); i++) {
-        std::cout << "client [" << i << "]:\n";
-        std::cout << "   start: " << rev_P.at(i).getStartDate() << '\n';
-        std::cout << "   end: " << rev_P.at(i).getEndDate() << '\n';
-        std::cout << "   pay: " << rev_P.at(i).getAmount() << '\n';
-    }
 }
+
+
+
+
 
 // Topological Sorting
 std::vector<int> Graph::topSort() {
@@ -141,7 +122,7 @@ std::vector<int> Graph::topSort() {
 
     while (!q.empty()) {
         n = q.front();
-        std::cout << n << " ";
+//        std::cout << n << " ";
         for(int i=0; i<(int)nodes[n].size(); i++) {
             edgeCount[nodes[n][i]]--;
             if (edgeCount[nodes[n][i]] == 0)
